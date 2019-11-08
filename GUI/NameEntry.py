bb8abf6abeccb23051dede210ec324cf45f9b4e7
Jack Turner <the.jack.turner@gmail.com>
2019-11-08 17:31:39 +0000

    .

--- GUI/NameEntry.py
+++ GUI/NameEntry.py
@@ -1,15 +1,25 @@
 import tkinter as tk
 from tkinter import ttk
 
-root = tk.Tk()
-root.geometry("150x50")
-root.eval('tk::PlaceWindow %s center' % root.winfo_pathname(root.winfo_id()))
+class NameEntry:
+    def __init__(self):
+        self.root =tk.Tk()
+        self.root.geometry("150x95")
+        self.root.eval('tk::PlaceWindow %s center' % self.root.winfo_pathname(self.root.winfo_id()))
 
-label = ttk.Label(root, text="What is your name? ")
-label.place(relx=0.5, rely=0.25, anchor=tk.CENTER)
+        self.label = ttk.Label(self.root, text="What is your name? ")
+        self.label.place(relx=0.5, rely=0.15, anchor=tk.CENTER)
 
+        self.entry = ttk.Entry(self.root)
+        self.entry.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
 
-entry = ttk.Entry(root)
-entry.place(relx=0.5, rely=0.65, anchor=tk.CENTER)
+        self.button = ttk.Button(self.root, text='Enter', command=self.getName)
+        self.button.place(relx=0.5, rely=0.7, anchor=tk.CENTER)
 
-root.mainloop()
+    def getName(self):
+        print(self.entry.get())
+        return self.entry.get()
+
+nameEntry = NameEntry()
+
+nameEntry.root.mainloop()
